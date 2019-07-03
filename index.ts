c615bf3aa753c09250ccc7f6e7c3c4c9ae49f35b
Marc Brüderlin <m.bruederlin@weiseundstark.de>
2019-07-03 12:33:37 +0200

    feat: implement react-native-screens for better performance

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { AppRegistry } from 'react-native'
+import { useScreens } from 'react-native-screens'
 
 import App from './src/App'
 import { name as appName } from './app.json'
+
+useScreens()
 
 AppRegistry.registerComponent(appName, () => App)
