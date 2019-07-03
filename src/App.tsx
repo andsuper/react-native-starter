bc7d6813f2bf85708599597f531dbe844467978d
Marc Brüderlin <m.bruederlin@weiseundstark.de>
2019-07-03 12:45:47 +0200

    feat: implement mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import { Provider } from 'mobx-react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
+
+import stores from './stores'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,11 +13,13 @@ interface Props {}
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider {...stores}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      </Provider>
     )
   }
 }
