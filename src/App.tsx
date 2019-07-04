160a4e868fdf7d8c7c1d4478fb7a8d045bbb5a9e
Marc Brüderlin <m.bruederlin@weiseundstark.de>
2019-07-04 14:15:58 +0200

    chore: add pre-commit hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { Provider } from 'mobx-react'
 import Navigator from './components/Navigation'
 import stores from './stores'
 
-interface Props {}
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <Provider {...stores}>
