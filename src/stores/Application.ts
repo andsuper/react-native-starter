a68b400c70f89eb5381aec4d3f16befc14d69a5c
Marc Brüderlin <m.bruederlin@weiseundstark.de>
2019-07-17 16:06:40 +0200

    feat: implement basic application store

--- src/stores/Application.ts
+++ src/stores/Application.ts
@@ -1,5 +1,7 @@
-import { observable } from 'mobx'
+import { action, observable, reaction } from 'mobx'
 
+import i18n from '../i18n'
+import { getDeviceLanguage } from '../utils'
 import { IRootStore } from './index'
 
 enum Theme {
@@ -22,6 +24,25 @@ class ApplicationStore implements IApplicationStore {
 
   constructor(rootStore: IRootStore) {
     this.rootStore = rootStore
+
+    this.hydrateStore()
+
+    reaction(() => this.language, language => i18n.setLanguage(language))
+  }
+
+  /**
+   * Sets the app language.
+   *
+   * @param {string} language
+   * @returns {void}
+   */
+  @action setLanguage(language: string): void {
+    this.language = language
+  }
+
+  private hydrateStore(): void {
+    // console.log('ApplicationStore --> hydrate')
+    this.setLanguage(getDeviceLanguage())
   }
 }
 
