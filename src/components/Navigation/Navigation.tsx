7da6401e0b972fba294f464b47c3ee62715ee599
Marc Brüderlin <m.bruederlin@weiseundstark.de>
2019-07-18 15:29:52 +0200

    refactor: simplify navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,40 +6,30 @@ import i18n from '../../i18n'
 import HomeScreen from '../../screens/Home'
 import SettingsScreen from '../../screens/Settings'
 
-export default createBottomTabNavigator(
+const Navigation = createBottomTabNavigator(
   {
     Home: {
       screen: HomeScreen,
       navigationOptions: {
         tabBarLabel: i18n.formatString(i18n.home.tabLabel),
-        // tabBarIcon:
+        tabBarIcon: ({ tintColor }: { tintColor: string }) => (
+          <Icon name="home" color={tintColor} />
+        ),
       },
     },
     Settings: {
       screen: SettingsScreen,
       navigationOptions: {
         tabBarLabel: i18n.formatString(i18n.settings.tabLabel),
+        tabBarIcon: ({ tintColor }: { tintColor: string }) => (
+          <Icon name="settings" color={tintColor} />
+        ),
       },
     },
   },
   {
     initialRouteName: 'Home',
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, horizontal, tintColor }) => {
-        const { routeName } = navigation.state
-        const key = getIconKey(routeName)
-
-        return <Icon name={key} color={tintColor || undefined} />
-      },
-    }),
   },
 )
 
-function getIconKey(route: string): string {
-  switch (route) {
-    case 'Settings':
-      return 'settings'
-    default:
-      return 'home'
-  }
-}
+export default Navigation
